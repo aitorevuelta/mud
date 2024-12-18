4f564044f333c8145eb6aadee0b0f825b6e7ac41
Aitor Revuelta Garrido <aitor.revuelta@alumni.mondragon.edu>
2024-12-18 11:51:28 +0100

    texturemanager creado

--- main/core/texturemanager.c
+++ main/core/texturemanager.c
@@ -2,6 +2,15 @@
 #include <stdbool.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <global.h>
 #include <utils.h>
 #include <texturemanager.h>
+
+SDL_Texture* loadTexture(const char* filePath, SDL_Renderer* renderer) {
+    SDL_Texture* texture = NULL;
+    SDL_Surface* surface = IMG_Load(filePath);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return texture;
+}
--- main/core/texturemanager.h
+++ main/core/texturemanager.h
@@ -1,4 +1,11 @@
 #ifndef TEXTUREMANAGER_H
 #define TEXTUREMANAGER_H
 
+typedef struct LOADEDTEXTURES_S {
+    int id;
+    SDL_Texture* texture;
+} LOADEDTEXTURES;
+
+SDL_Texture* loadTexture(const char* filePath, SDL_Renderer* renderer);
+
 #endif
