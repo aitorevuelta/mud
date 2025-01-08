37f1727b10c75d91c6e72d1cdc914176681284a2
asiero12 <asier.otxoa@alumni.mondragon.edu>
2025-01-08 18:01:33 +0100

    mejorau

---
+++ main/core/game/game.c
@@ -0,0 +1,18 @@
+#include <global.h>
+
+#include <game.h>
+
+void initialize_game()
+{
+
+}
+
+void update_game()
+{
+
+}
+
+void cleanup_game()
+{
+
+}
---
+++ main/core/game/game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+
+void initialize_game();
+
+void update_game();
+
+void cleanup_game();
+
+
+
+
+#endif // GAME_H
