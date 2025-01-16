fd8a9430388eb94cef29db72e4f09fe4c6f8d192
Aitor Revuelta Garrido <aitor.revuelta@alumni.mondragon.edu>
2025-01-16 08:25:07 +0100

    arregla esto

--- main/core/graphics/game_render.c
+++ main/core/graphics/game_render.c
@@ -18,18 +18,21 @@ void renderMap(SDL_Renderer *renderer, IMAGES *loadedImages, FONTS *loadedFonts,
 
 void renderUI(SDL_Renderer *renderer, IMAGES *loadedImages, FONTS *loadedFonts, GAMEINFO gameInfo)
 {
-    
+    renderPlayerUI(renderer, loadedImages, loadedFonts, gameInfo.numPlayers, gameInfo.players);
 
     //player ui
-    int players = gameInfo.numPlayers;
+
+}
+
+void renderPlayerUI(SDL_Renderer *renderer, IMAGES *loadedImages, FONTS *loadedFonts, int numPlayers, PLAYER* playerinfo) {
     int espacio = 5;
     int playerHeight = 10;
     
     if (players > 0) {
-        int totalHeight = (players * playerHeight) + ((players - 1) * espacio);
+        int totalHeight = (numPlayers * playerHeight) + ((numPlayers - 1) * espacio);
         int startY = 50 + (totalHeight / 2);
         
-        for (int i = 0; i < players; i++) {
+        for (int i = 0; i < numPlayers; i++) {
             int currentY = startY - (i * (playerHeight + espacio));
             if (i == 2) {
                  renderTextureRelative(renderer, loadedImages[1].texture, 5, 89, currentY);
@@ -37,8 +40,4 @@ void renderUI(SDL_Renderer *renderer, IMAGES *loadedImages, FONTS *loadedFonts, 
             renderTextureRelative(renderer, loadedImages[0].texture, 7, 95, currentY);
         }
     }
-}
-
-void renderPlayerUI(SDL_Renderer *renderer, IMAGES *loadedImages, FONTS *loadedFonts, PLAYER playerinfo) {
-        renderTextureRelative(renderer, loadedImages[0].texture, 7, 93, 30);
 }
