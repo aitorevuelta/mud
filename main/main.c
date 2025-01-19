95c0e61fc26d896c7db992fb80c4cd25b91ffeba
asiero12 <asier.otxoa@alumni.mondragon.edu>
2025-01-19 16:21:39 +0100

    a

--- main/main.c
+++ main/main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     config.selectedVolume = 0;
 
     bool is_running = init_sdl(sdl, config);
-    LoadAssets(sdl.renderer, &loadedAssets, gameState);
+    LoadAssets(sdl.renderer, loadedAssets, gameState);
     
     do {
         is_running = process_events(sdl.window, &controls, &config);
